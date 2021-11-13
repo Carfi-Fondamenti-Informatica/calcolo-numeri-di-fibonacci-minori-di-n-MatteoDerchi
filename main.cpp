332babb769cfecb9ad05da7137eed5dcc68648fb
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-11-13 23:26:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 
 int main() {
-    int n=1, a=0, b=1, c= a+b;
+    int n=1, a=0, b=1, c;
     cin>>n;
     if (n==0) {
         cout<<" ";
     }else{
         cout<< "1" <<endl;
-        cout<< "1" <<endl;
         for (int i=0; i<n ;i++) {
+            c = a + b;
             a = b;
             b = c;
-            c = a + b;
             if (c<=n){
                 cout << c << endl;
             }else{
