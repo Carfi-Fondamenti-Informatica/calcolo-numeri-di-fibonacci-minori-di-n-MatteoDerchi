ec44ca7dc4216958558f061b08f6e2fffcc21cbd
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-11-13 23:04:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 
 int main() {
-    int n=0, a=0, b=1, c=a+b;
+    int n=1, a=0, b=1, c= a+b;
     cin>>n;
-
-    cout<< b <<endl;
-    for (int i=2; i<(n+1) ;i++) {
-        if (c<=n){
-            cout << c << endl;
+    if (n==0) {
+        cout<<" ";
+    }else{
+        cout<< "1" <<endl;
+        cout<< "1" <<endl;
+        for (int i=0; i<n ;i++) {
             a = b;
             b = c;
             c = a + b;
-        }else{
-            break;
-        }
-
+            if (c<=n){
+                cout << c << endl;
+            }else{
+                break;
+            }
+    }
         }
 
     return 0;
